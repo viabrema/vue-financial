6a22658ad376679dc469d388612a7432bf639ab8
viabrema <viabrema@gmail.com>
2021-05-19 10:30:57 -0300

    entradas e saídas de valores

--- financial-app/src/App.vue
+++ financial-app/src/App.vue
@@ -1,25 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ data.month }}/{{data.year}}</h1>
+    <h2>Entradas</h2>
+    <ul>
+      <li v-for="(entry, index) in data.entries" :key="index">
+        <span><input type="text" v-model="entry.name"></span>
+        <span><input type="text" v-model="entry.value"></span>
+      </li>
+      <li><span>Total</span><span>{{entryTotal}}</span></li>
+    </ul>
+
+    <h2>Saídas</h2>
+    <ul>
+      <li v-for="(expense, index) in data.expenses" :key="index">
+        <span><input type="text" v-model="expense.name"></span>
+        <span><input type="text" v-model="expense.value"></span>
+      </li>
+      <li><span>Total</span><span>{{expenseTotal}}</span></li>
+    </ul>
+
+    <h2>Saldo Total: {{entryTotal - expenseTotal}}</h2>
+
+    <entry @cadastre="cadastre"></entry>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, computed } from "vue";
+import Entry from './components/Entry.vue';
+
 
 export default {
-  name: 'App',
+  name: "App",
+
+  setup(props) {
+    const data = reactive({
+      month: 5,
+      year: 2021,
+      entries: [{ name: "Inicial", value: 0.0 }],
+      expenses: [{ name: "Inicial", value: 0.0 }],
+    });
+
+
+    const entryTotal = computed(() => {
+      let total = 0;
+      data.entries.forEach(e => {
+        total = Number(total) + Number(e.value);
+      });
+      return total;
+    })
+
+    const expenseTotal = computed(() => {
+      let total = 0;
+      data.expenses.forEach(e => {
+        total = Number(total) + Number(e.value);
+      });
+      return total;
+    })
+
+    function cadastre(res){
+      if(res.type == 1){
+        data.entries.push(res.data);
+      }else{
+        data.expenses.push(res.data);
+      }
+    }
+
+    return {
+      data,
+      cadastre,
+      entryTotal,
+      expenseTotal
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Entry
+
+  },
+};
 </script>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<style lang="stylus"></style>
---
+++ financial-app/src/components/Entry.vue
@@ -0,0 +1,46 @@
+<template>
+
+<div>
+    <label for="name">Nome:</label>
+    <input id="cadastreName" name="name" type="text" v-model="data.name">
+    <label for="value">Valor:</label>
+    <input name="value" type="number" v-model="data.value">
+    <select v-model="data.type">
+        <option value="1">Entrada</option>
+        <option value="2">Saída</option>
+    </select>
+    <button @click="cadastre">Cadastrar</button>
+</div>
+    
+</template>
+
+
+<script>
+import {reactive, ref} from "vue";
+
+export default {
+    setup(props, context) {
+        const data = reactive({
+            name: "",
+            value: "",
+            type: 2
+        });
+
+        function cadastre(){
+            context.emit("cadastre", {data:{name: data.name, value: data.value}, type: data.type});
+            data.name = "";
+            data.value = "";
+            document.getElementById("cadastreName").focus();;
+        }
+
+        return{
+            data,
+            cadastre
+        }
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
